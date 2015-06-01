c434e75ffa918b0451823ea68f3ae9043fca417c
Michael Burson <michael.burson@razoroccam.com>
2015-06-01 21:21:32 +0100

    Serve files from controller

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ var bodyParser = require( 'body-parser' )
 
 var app = express()
 app.use( bodyParser.json() )
-app.use( '/api/posts', require( './controllers/api/posts' ) )
 
-app.get( '/', function( req, res ) {
-    res.sendfile( 'layouts/posts.html' )
-})
+app.use( '/api/posts', require( './controllers/api/posts' ) )
+app.use( require( './controllers/api/static' ) )
 
 app.listen( 3000, function() {
     console.log( 'Server listening on', 3000 )
