0725faf280df8e898250bc5fad02fbccaa8810c6
Michael Burson <michael.burson@razoroccam.com>
2015-06-06 09:51:06 +0100

    Typo in model resolved

--- models/user.js
+++ models/user.js
@@ -3,4 +3,4 @@ var user = db.Schema({
     username: { type: String, required: true },
     password: { type: String, required: true, select: false }
 })
-module.export = db.model( 'User', user )
+module.exports = db.model( 'User', user )
