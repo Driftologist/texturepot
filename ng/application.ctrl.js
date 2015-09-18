0b9170685c7d5c2e927dc406bdabba86a9527b53
Michael Burson <michael.burson@razoroccam.com>
2015-09-18 18:01:01 +0100

    Find existing user and display logged in views

--- ng/application.ctrl.js
+++ ng/application.ctrl.js
@@ -1,8 +1,12 @@
 angular.module( 'app' )
-.controller( 'ApplicationCtrl', function( $scope, UserSvc ) {
+.controller( 'ApplicationCtrl', function( $scope, UserSvc, jwtHelper ) {
     $scope.$on( 'login', function( _, user ) {
         $scope.currentUser = user
     })
+    if (window.localStorage.token) {
+        $scope.$emit( 'login', jwtHelper.decodeToken(window.localStorage.token) )
+        UserSvc.getUser()
+    }
     $scope.logout = function() {
         if ($scope.currentUser) {
             delete $scope.currentUser
