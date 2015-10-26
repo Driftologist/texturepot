ad6ade6816f4751d7cea1842ed2ac0c71329c48e
Michael Burson <michael.burson@razoroccam.com>
2015-10-26 14:21:31 +0000

    Setup protractor and write first test

---
+++ test/e2e/making_a_post.spec.js
@@ -0,0 +1,8 @@
+describe('making a post', function() {
+    it('logs in and creates a new post', function() {
+        browser.get('http://localhost:3000')
+        // click login
+        // fill out and submit login form
+        // submit a new post on the posts page
+    })
+})
