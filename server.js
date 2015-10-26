dd05cafb908b7aa768b4ddf27ea08b5e9ae46db5
Michael Burson <michael.burson@razoroccam.com>
2015-10-26 16:30:29 +0000

    Refactor server to allow for better testing

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 var express = require( 'express' )
-var bodyParser = require( 'body-parser' )
-
+//var logger = require( 'morgan' )
 var app = express()
-app.use( bodyParser.json() )
-app.use( require( './auth' ) )
 
-app.use( '/api/posts', require( './controllers/api/posts' ) )
-app.use( '/api/sessions', require( './controllers/api/sessions' ) )
-app.use( '/api/users', require( './controllers/api/users' ) )
-app.use( require( './controllers/static' ) )
-// equivalent to: app.use( '/', require( './controllers/static' ) )
+//app.use( logger( dev ) )
+app.use( require( './controllers' ) )
 
 var port = process.env.PORT || 3000
 
