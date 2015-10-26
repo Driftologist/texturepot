24ca2eb8ed4dcfcdc5bab69e0eb78b2ee55a50b9
Michael Burson <michael.burson@razoroccam.com>
2015-10-26 15:23:32 +0000

    Set e2e tests to run with or without a local server

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ app.use( bodyParser.json() )
 app.use( require( './auth' ) )
 
 app.use( '/api/posts', require( './controllers/api/posts' ) )
+app.use( '/api/sessions', require( './controllers/api/sessions' ) )
+app.use( '/api/users', require( './controllers/api/users' ) )
 app.use( require( './controllers/static' ) )
 // equivalent to: app.use( '/', require( './controllers/static' ) )
 
-app.use( '/api/sessions', require( './controllers/api/sessions' ) )
-app.use( '/api/users', require( './controllers/api/users' ) )
+var port = process.env.PORT || 3000
 
-app.listen( 3000, function() {
-    console.log( 'Server listening on', 3000 )
+app.listen( port, function() {
+    console.log( 'Server listening on', port )
 })
