7f50a7b8443084e538d47273ba9b93197c0d5014
Michael Burson <michael.burson@razoroccam.com>
2015-10-26 16:35:29 +0000

    Remove commented code

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require( 'express' )
-//var logger = require( 'morgan' )
 var app = express()
 
-//app.use( logger( dev ) )
 app.use( require( './controllers' ) )
 
 var port = process.env.PORT || 3000
