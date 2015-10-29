45b82e187397c691d7cb912899707fdba6818cad
Michael Burson <michael.burson@razoroccam.com>
2015-10-29 10:51:38 +0000

    Allow the app to load on the two other entry pages

--- controllers/static.js
+++ controllers/static.js
@@ -8,5 +8,11 @@ router.use( express.static( __dirname + '/../templates' ) )
 router.get( '/', function( req, res ) {
     res.sendfile( 'layouts/app.html' )
 })
+router.get( '/login', function( req, res ) {
+    res.sendfile( 'layouts/app.html')
+})
+router.get( '/register', function( req, res ) {
+    res.sendfile( 'layouts/app.html')
+})
 
 module.exports = router
